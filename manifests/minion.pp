7487c1752a5c25cd9d317f8aa570ddde9c52c227
Stein <mst43@ostml0231493.bskyb.com>
2017-01-09 18:12:06 +0000

    add in minion docker ordering

--- manifests/minion.pp
+++ manifests/minion.pp
@@ -7,9 +7,14 @@ class kubernetes::minion($master_name=undef, $minion_name=undef, $alternate_flan
   file{'/etc/kubernetes/kubelet':
     content => template('kubernetes/kubelet.erb')
   }~>
-  service{['kube-proxy','kubelet', 'docker', 'flanneld']:
+  service{['kube-proxy','kubelet', 'flanneld']:
+    ensure => running,
+    enable => true,
+  }~>
+  service{'docker':
     ensure => running,
     enable => true,
   }
+
 }
 
